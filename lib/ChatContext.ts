53a3a30f5cf6fdaf505af75c0e345a144da2c960
gaojunkang <gaojunkang@xiaohongshu.com>
2023-03-11 15:34:07 +0800

    fix: retry 重复 append context 的问题

--- lib/ChatContext.ts
+++ lib/ChatContext.ts
@@ -2,7 +2,7 @@ import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai"
 import { MassageContext } from "./MessageContext.js"
 
 export interface ChatContext {
-  send(msg: ChatCompletionRequestMessage, retry?: number): Promise<string>
+  send(msg: ChatCompletionRequestMessage, retry?: number, isRetry?: boolean): Promise<string>
   resetConversition(): Promise<void>
 }
 
@@ -19,9 +19,9 @@ export const createChatContext = ({
   const openai = new OpenAIApi(new Configuration({ apiKey: secret }));
   let msgContext = new MassageContext(messages ?? [], maxToken)
   return {
-    async send(msg: ChatCompletionRequestMessage, retry: number = 2): Promise<string> {
+    async send(msg: ChatCompletionRequestMessage, retry: number = 2, isRetry: boolean = false): Promise<string> {
       try {
-        msgContext.push(msg)
+        if (!isRetry) msgContext.push(msg)
         const res = await openai.createChatCompletion({
           model: model ?? 'gpt-3.5-turbo',
           messages: msgContext.getMessagesClone(),
@@ -32,7 +32,7 @@ export const createChatContext = ({
         return message?.content ?? ''
       } catch (e) {
         if (retry > 0 && (e as any)?.response?.status === 429) {
-          return this.send(msg, retry - 1)
+          return this.send(msg, retry - 1, true)
         }
         throw e
       }
